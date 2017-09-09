3a6b1da2cbd79768c596fbdd7002fca608bce4e2
yani-valeva <yani.valeva@gmail.com>
2017-09-09 13:04:51 +0300

    Update CurrencyConverter.py

--- Calculations/CurrencyConverter.py
+++ Calculations/CurrencyConverter.py
@@ -1,37 +1,37 @@
 money = float(input())
-inputCurrency = input()
-outputCurrency = input()
+input_currency = input()
+output_currency = input()
 bgn = 0
 eur = 0
 gbp = 0
 usd = 0
 
-if inputCurrency == 'USD':
+if input_currency == 'USD':
     usd = money
     bgn = money * 1.79549
     eur = bgn / 1.95583
     gbp = bgn / 2.53405
-elif inputCurrency == 'BGN':
+elif input_currency == 'BGN':
     bgn = money
     usd = money / 1.79549
     eur = money / 1.95583
     gbp = money / 2.53405
-elif inputCurrency == 'EUR':
+elif input_currency == 'EUR':
     eur = money
     bgn = money * 1.95583
     usd = bgn / 1.79549
     gbp = bgn / 2.53405
-elif inputCurrency == 'GBP':
+elif input_currency == 'GBP':
     gbp = money
     bgn = money * 2.53405
     usd = bgn / 1.79549
     eur = bgn / 1.95583
 
-if outputCurrency == 'BGN':
+if output_currency == 'BGN':
     print('{0} BGN'.format(round(bgn, 2)))
-elif outputCurrency == 'USD':
+elif output_currency == 'USD':
     print('{0} USD'.format(round(usd, 2)))
-elif outputCurrency == 'EUR':
+elif output_currency == 'EUR':
      print('{0} EUR'.format(round(eur, 2)))
-elif outputCurrency == 'GBP':
+elif output_currency == 'GBP':
     print('{0} GBP'.format(round(gbp, 2)))
